94c016419b5dd7d15503a3c286a6d7ca05ae6f67
facundosarabia99 <facundosarabia@gmail.com>
2022-04-26 00:41:50 -0300

    .

---
+++ PlantillaDeMotores/Pug/routers/routerProductos.js
@@ -0,0 +1,28 @@
+const {Router} = require('express')
+
+const routerProductos = Router()
+const productos = []
+
+routerProductos.get('/', (req, res) => {
+    res.render('formulario')
+})
+
+routerProductos.get('/productos', (req, res) => {
+    res.render('historial', {productos})
+})
+
+routerProductos.post('/productos', (req, res) => {
+    if (!productos.length) {
+        productos.id = 1
+    } else {
+        productos.id = productos.length + 1
+    } 
+    productos.push(req.body)
+    console.log(`El id de este producto es ${productos.id}`)
+    res.redirect('/')
+})
+
+
+
+
+module.exports = routerProductos;
